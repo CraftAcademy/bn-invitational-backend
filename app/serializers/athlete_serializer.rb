6399fad4434ff7a8a2d6ffbb77813235ee664944
Holger_Tidemand <tidemand.holger@gmail.com>
2018-04-03 20:27:17 +0200

    refactored show serializer to support heruko url

--- app/serializers/athlete_serializer.rb
+++ app/serializers/athlete_serializer.rb
@@ -1,12 +1,11 @@
 class AthleteSerializer < ActiveModel::Serializer
-  include Rails.application.routes.url_helpers
+  #include Rails.application.routes.url_helpers
   attributes :name, :link
 
   def link
-      "http://localhost:3000/#{object.id}"
-    #"#{api_v1_athletes_url(host: 'http://localhost:3000')}/#{object.id}"
+    "https://votingapi.herokuapp.com/#{object.id}"
     #tried to use URL helpers
-    #api_v1_athletes_url(object) = http://localhost:3000/api/v1/athletes.1
+    #api_v1_athletes_url(object, host: 'http://localhost:3000' ) = http://localhost:3000/api/v1/athletes.1
     #Need help to understand this better
   end
 end
