0ccc93fad3b0308f157f2841b6e2a6b437738874
larathordar <lara-thordar@hotmail.com>
2018-04-03 14:30:34 +0200

    adds jsonapi-rspec

--- Gemfile
+++ Gemfile
@@ -22,3 +22,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
+
+group :test do
+  gem 'jsonapi-rspec', require: false
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'jsonapi/rspec'
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -11,4 +12,5 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include JSONAPI::RSpec
 end
