f79d98184a3d65981166ce96ca4b9242b5a7c819
larathordar <lara-thordar@hotmail.com>
2018-04-04 14:12:12 +0200

    results can be displayed

--- app/controllers/api/v1/results_controller.rb
+++ app/controllers/api/v1/results_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ResultsController < ApplicationController
     def index
       results = Result.all
-      render json: results, status: :ok 
+      results.sort_by {|result| result.score}
+      binding.pry
+      render json: results, status: :ok
     end
 end
