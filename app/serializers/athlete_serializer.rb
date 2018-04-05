241b0e27fe8dc0ad213d230c818926544667027b
larathordar <lara-thordar@hotmail.com>
2018-04-05 20:17:13 +0200

    adds image to serializer

--- app/serializers/athlete_serializer.rb
+++ app/serializers/athlete_serializer.rb
@@ -1,6 +1,6 @@
 class AthleteSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :name, :link, :home
+  attributes :name, :link, :home, :image
 
   def link
     api_v1_athlete_url(object)
