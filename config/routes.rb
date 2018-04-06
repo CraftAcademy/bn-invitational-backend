8559804820075705a4b5811e996bebdeb7abbc35
bollen <david.boll@me.com>
2018-04-06 20:21:21 +0200

    Updates routes to include destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-namespace :api do
-  namespace :v1 do
-    resources :athletes, only: [:index, :show, :update, :create]
-    resources :results, only: [:index]
-    mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :api do
+    namespace :v1 do
+      resources :athletes, expept: [:new, :edit]
+      resources :results, only: [:index]
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
   end
 end
