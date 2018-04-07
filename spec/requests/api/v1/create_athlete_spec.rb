57f6c17a28479c2bb8a1a6602a1bd127fdf94f5c
kalle <kmthorsnes@gmail.com>
2018-04-07 19:27:23 +0200

    Add before do block in create_athlete_spec.rb

--- spec/requests/api/v1/create_athlete_spec.rb
+++ spec/requests/api/v1/create_athlete_spec.rb
@@ -6,30 +6,27 @@ RSpec.describe Api::V1::AthletesController, type: :request do
     let(:credentials) { user.create_new_auth_token }
     let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
 
-    it 'should return success message if athlete is created' do
+    before do
       post '/api/v1/athletes', params: {
         name: 'Holger Tidemand', age: 26, home: 'Onsala'
       }, headers: headers
+    end
 
+    it 'Should return success message if athlete is created' do
       expect(object).to eq 'Athlete successfully created!'
     end
 
-    it 'should create a result for the athlete' do
-      post '/api/v1/athletes', params: {
-        name: 'Holger Tidemand', age: 26, home: 'Onsala'
-      }, headers: headers
-
+    it 'Should create a result for the athlete' do
       result = Result.first
       athlete = Athlete.first
       expect(result.athlete_id).to eq athlete.id
     end
 
-    it 'should return success message if athlete is created' do
+    it 'Should return error message if not all fields are entered' do
       post '/api/v1/athletes', params: {
         name: 'Holger Tidemand', age: 26
       }, headers: headers
-
-      expect(object).to eq 'Please fill in all fields.'
+      expect(object).to eq 'Plea  se fill in all fields.'
     end
   end
 end
