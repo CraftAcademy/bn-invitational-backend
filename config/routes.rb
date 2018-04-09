1a5e9863c11c4e0e4306e2de34e5be339110d8ce
Holger_Tidemand <tidemand.holger@gmail.com>
2018-04-09 21:33:15 +0200

    clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root controller: :athletes, action: :index
 
-  resources :athletes
+  resources :athletes, only: [:index, :create]
   put 'toggle/:id', to: 'athletes#toggle', as: :toggle_voting
 
   namespace :api do
