a0bb5f22934f517acbe19929b0adc2217cebc0b6
Holger_Tidemand <tidemand.holger@gmail.com>
2018-04-10 20:27:59 +0200

    adds NoticicationHandler modoule

---
+++ app/controllers/concerns/notifications_handler.rb
@@ -0,0 +1,18 @@
+module NotificationsHandeler
+  def push_notification(title, body)
+    params = {"app_id" => Rails.application.credentials.dig(:onesignal, :app_id),
+            "headings" => {"en" => "title"},
+            "contents" => {"en" => "English Message"},
+            "included_segments" => ["All"]}
+    uri = URI.parse('https://onesignal.com/api/v1/notifications')
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Post.new(uri.path,
+                              'Content-Type'  => 'application/json;charset=utf-8',
+                              'Authorization' => Rails.application.credentials.dig(:onesignal, :api_key))
+      request.body = params.to_json
+      response = http.request(request)
+      puts response.body
+    end
+end
