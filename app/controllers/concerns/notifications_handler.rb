a5b17c5c4d2f2dc680cbd5339fec7692667f2184
Holger_Tidemand <tidemand.holger@gmail.com>
2018-04-11 10:01:29 +0200

    refactored notification handeler

--- app/controllers/concerns/notifications_handler.rb
+++ app/controllers/concerns/notifications_handler.rb
@@ -9,15 +9,13 @@ module NotificationsHandler
   def push_notification(obj)
     one_signal_url = 'https://onesignal.com/api/v1/notifications'
     params = eval(file_data('notifications_params'))
+    headers = eval(file_data('notifications_headers'))
 
     uri = URI.parse(one_signal_url)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
 
-    request = Net::HTTP::Post.new(uri.path,
-                              'Content-Type'  => 'application/json;charset=utf-8',
-                              'Authorization' => Rails.application.credentials.dig(:onesignal, :api_key))
-
+    request = Net::HTTP::Post.new(uri.path, headers)
     request.body = params.to_json
     http.request(request)
   end
